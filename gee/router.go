9c7f370186412abd2e904bbc9c598943e05ee798
kongnull <zendchina@outlook.com>
2023-04-24 18:33:58 +0800

    路由匹配

--- gee/router.go
+++ gee/router.go
@@ -17,11 +17,12 @@ func newRouter() *router {
 		handlers: make(map[string]HandlerFunc),
 	}
 }
+
+// Only one * is allowed
 func parsePattern(pattern string) []string {
 	vs := strings.Split(pattern, "/")
 
 	parts := make([]string, 0)
-
 	for _, item := range vs {
 		if item != "" {
 			parts = append(parts, item)
@@ -46,6 +47,7 @@ func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
 	r.roots[method].insert(pattern, parts, 0)
 	r.handlers[key] = handler
 }
+
 func (r *router) getRoute(method string, path string) (*node, map[string]string) {
 	searchParts := parsePattern(path)
 	params := make(map[string]string)
