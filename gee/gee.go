218c4f310b0b42949eb2aceed15ee9153631d7d0
kongnull <zendchina@outlook.com>
2023-04-24 11:55:32 +0800

    构建router

--- gee/gee.go
+++ gee/gee.go
@@ -1,29 +1,28 @@
 package gee
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type HandleFunc func(w http.ResponseWriter, r *http.Request)
+type HandlerFunc func(*Context)
 
 type Engine struct {
-	router map[string]HandleFunc
+	router *router
 }
 
-func (engine *Engine) addRoute(method string, patter string, handler HandleFunc) {
-	key := method + "-" + patter
-	engine.router[key] = handler
-}
 func New() *Engine {
-	return &Engine{router: make(map[string]HandleFunc)}
+	return &Engine{router: newRouter()}
 }
 
-func (engine *Engine) Get(pattern string, handler HandleFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (engine *Engine) addRoute(method string, patter string, handler HandlerFunc) {
+	engine.router.addRoute(method, patter, handler)
 }
 
-func (engine *Engine) Post(patter string, handler HandleFunc) {
+func (engine *Engine) Get(pattern string, handler HandlerFunc) {
+	engine.router.addRoute("GET", pattern, handler)
+}
+
+func (engine *Engine) Post(patter string, handler HandlerFunc) {
 	engine.addRoute("POST", patter, handler)
 }
 
@@ -32,13 +31,6 @@ func (engine *Engine) Run(addr string) (err error) {
 }
 
 func (engine *Engine) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	key := request.Method + "-" + request.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(writer, request)
-	} else {
-		_, err := fmt.Fprintf(writer, "404 NOT FOUND: %s\n", request.URL)
-		if err != nil {
-			return
-		}
-	}
+	c := newContext(writer, request)
+	engine.router.handle(c)
 }
--- gee/router.go
+++ gee/router.go
@@ -6,15 +6,15 @@ import (
 )
 
 type router struct {
-	handlers map[string]HandleFunc
+	handlers map[string]HandlerFunc
 }
 
 func newRouter() *router {
-	return &router{handlers: make(map[string]HandleFunc)}
+	return &router{handlers: make(map[string]HandlerFunc)}
 }
 
-func (r *router) addRoute(method string, pattern string, handler HandleFunc) {
-	log.Printf("Route %4s-%s", method, pattern)
+func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
+	log.Printf("Route %4s - %s", method, pattern)
 	key := method + "-" + pattern
 	r.handlers[key] = handler
 }
